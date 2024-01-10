264befab07ebbb64d656cdd042c909470cf44af6
Stanislav Kostenich <52426715+ukrsite@users.noreply.github.com>
2024-01-10 02:40:43 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,28 +18,17 @@ module "tls_private_key" {
   source = "github.com/den-vasyliev/tf-hashicorp-tls-keys"
 }
 
-provider "flux" {
-  kubernetes = {
-    host                   = module.gke_cluster.config_host
-    token                  = module.gke_cluster.config_token
-    cluster_ca_certificate = module.gke_cluster.config_ca
-  }
-  git = {
-    url = "https://github.com/${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}.git"
-    http = {
-      username = "git"
-      password = var.GITHUB_TOKEN
-    }
-  }
+module "flux_bootstrap" {
+  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=gke_auth"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  config_host       = module.gke_cluster.config_host
+  config_token      = module.gke_cluster.config_token
+  config_ca         = module.gke_cluster.config_ca
+  github_token      = var.GITHUB_TOKEN
 }
-
-resource "flux_bootstrap_git" "this" {
-  path = "clusters"
-}
-
 
 module "gke_workload_identity" {
-
   source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
   use_existing_k8s_sa = true
   name                = "kustomize-controller"
